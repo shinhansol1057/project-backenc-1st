a43a8dced56d8d016f1a06bb02faa121a7b150b4
shinhansol1057 <wkdqhrh010@naver.com>
2023-11-26 23:00:23 +0900

    Initial commit

---
+++ src/main/java/com/github/projectbackenc1st/ProjectBackenc1stApplication.java
@@ -0,0 +1,13 @@
+package com.github.projectbackenc1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackenc1stApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBackenc1stApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/projectbackenc1st/ProjectBackenc1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projectbackenc1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackenc1stApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
